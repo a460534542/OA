d47211deddb065ac931874ae2bc40b8c2d26d5bc
Administrator <Administrator@QAAWOGT2AW396W1>
2018-10-15 23:51:43 +0800

    2018/10/15

--- ssm Maven Webapp/src/main/java/com/bwf/controller/AffairModuleController.java
+++ ssm Maven Webapp/src/main/java/com/bwf/controller/AffairModuleController.java
@@ -1,11 +1,17 @@
 package com.bwf.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.bwf.entity.AffairModule;
+import com.bwf.entity.User;
+import com.bwf.service.IAffairModuleService;
 import com.bwf.service.IDepartmentService;
 
 @Controller
@@ -14,6 +20,9 @@ public class AffairModuleController {
 
 	@Autowired
 	IDepartmentService departmentService;
+	
+	@Autowired
+	IAffairModuleService affairModuleService;
 	
 	@GetMapping("show")
 	public String show(){
@@ -29,4 +38,19 @@ public class AffairModuleController {
 		return "affair_module/add";
 	}
 	
+	@PostMapping("doAdd")
+	public String doAdd(AffairModule affairModule , 
+			String[] optionName , 
+			Integer[] optionId , 
+			String[] optionData ,
+			Integer[] approverId ,
+			HttpSession session){
+		User user = ( User ) session.getAttribute("user");
+		affairModule.setAffairModuleProducerId( user.getUserId() );
+		
+		affairModuleService.add( affairModule ,optionName , optionId ,  optionData, approverId );
+		
+		
+		return "redirect: /affair_module/show";
+	}
 }
--- ssm Maven Webapp/src/main/java/com/bwf/controller/RosterController.java
+++ ssm Maven Webapp/src/main/java/com/bwf/controller/RosterController.java
@@ -3,6 +3,7 @@ package com.bwf.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributesModelMap;
 
 // 考勤管理
 @Controller
@@ -10,7 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class RosterController {
 
 	@GetMapping("show")
-	public String show(){
+	public String show(RedirectAttributesModelMap r){
+	
 		return "roster/show";
 	}
 }
